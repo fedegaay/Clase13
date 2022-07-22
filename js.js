6f17cbff19f317f039256a3d8eb6d9b0fa4119f8
FedeGaay <107083865+FedeGaay@users.noreply.github.com>
2022-07-21 21:05:35 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+
+function calcular(){
+    var num1 = prompt("ingrese el primer numero");
+    var num2 = prompt("ingrese el segundo numero");
+    resta = num1 - num2;
+    division = num1 / num2;
+    alert("La resta de los numeros es: " + resta + ". " + "\nLa division de los numeros es igual a: " + division);
+}
